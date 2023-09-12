d28606c4b31e7738a78e3f01db50f00b798c413c
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-09-12 11:00:21 +0300

    args

--- detector.py
+++ detector.py
@@ -1,9 +1,13 @@
 import argparse
 import cv2
+import pandas as pd
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', required=True, help="Image Path")
 args = vars(ap.parse_args())
 img_path = args['i']
 img = cv2.imread(img_path)
-print(img_path)
+# print(img_path)
+
+cols = ["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('colors.csv', names=cols, header=None)
